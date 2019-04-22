36099f48b529ce2e631907f12d9a5b8aa88d9bd6
Danny Martin <danielmartin0126@gmail.com>
2019-04-22 20:54:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function deleteFromDriverByKey(driver,key) {
   return newDriver
 }
 
-function destructivelyDeleteFromDriverByKey(driver,key)
+function destructivelyDeleteFromDriverByKey(driver,key){
   delete driver[key]
   return driver 
   }
